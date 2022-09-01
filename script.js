fce152e9dbddc31e55cbf673bb17510d59ffd492
Deee3 <dtran0418@gmail.com>
2022-09-01 09:16:12 -0400

    test 1

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your JS goes here
+
+function tile(color){
+let box = document.createElement('div')
+box.style.width = "11.1%";
+box.style.float = "left";
+box.style.paddingBottom = "11.1%";
+box.style.backgroundColor = color
+document.body.append(box);
+}
+
+
+for(let i = 0; i < 10000; i++){
+    if(i % 2 === 0){
+        tile('pink')
+    }
+    else if(i % 2 === 1){
+        tile('blue')
+    }
+}
